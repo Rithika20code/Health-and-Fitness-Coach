cc6b29d04f15c48a4716346b8bd3d0a73d72616c
Rithika20code <rithisrivi@gmail.com>
2026-03-11 22:09:20 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,50 +1,230 @@
-/**
- * RunAnywhere Web SDK - Core Package (Pure TypeScript)
- *
- * Backend-agnostic infrastructure for on-device AI in the browser.
- * This package has ZERO WASM — all inference binaries live in backend packages:
- *   - @runanywhere/web-llamacpp — LLM, VLM, embeddings, diffusion (ships racommons-llamacpp.wasm)
- *   - @runanywhere/web-onnx — STT, TTS, VAD (ships sherpa-onnx.wasm)
- *
- * @packageDocumentation
- *
- * @example
- * ```typescript
- * import { RunAnywhere } from '@runanywhere/web';
- * import { LlamaCPP } from '@runanywhere/web-llamacpp';
- * import { ONNX } from '@runanywhere/web-onnx';
- *
- * await RunAnywhere.initialize({ environment: 'development' });
- * await LlamaCPP.register();
- * await ONNX.register();
- * ```
- */
-// Main entry point
-export { RunAnywhere } from './Public/RunAnywhere';
-// Voice orchestration (cross-backend, uses provider interfaces)
-export { VoiceAgent, VoiceAgentSession, PipelineState } from './Public/Extensions/RunAnywhere+VoiceAgent';
-export { VoicePipeline } from './Public/Extensions/RunAnywhere+VoicePipeline';
-// Types
-export * from './types';
-// Foundation
-export { SDKError, SDKErrorCode, isSDKError } from './Foundation/ErrorTypes';
-export { SDKLogger, LogLevel } from './Foundation/SDKLogger';
-export { EventBus } from './Foundation/EventBus';
-// I/O Infrastructure (backend-agnostic capture/playback)
-export { AudioCapture } from './Infrastructure/AudioCapture';
-export { AudioPlayback } from './Infrastructure/AudioPlayback';
-export { AudioFileLoader } from './Infrastructure/AudioFileLoader';
-export { VideoCapture } from './Infrastructure/VideoCapture';
-// Infrastructure
-export { detectCapabilities, getDeviceInfo } from './Infrastructure/DeviceCapabilities';
-export { ModelManager } from './Infrastructure/ModelManager';
-export { OPFSStorage } from './Infrastructure/OPFSStorage';
-export { ExtensionRegistry } from './Infrastructure/ExtensionRegistry';
-export { ExtensionPoint, BackendCapability, ServiceKey } from './Infrastructure/ExtensionPoint';
-export { extractTarGz } from './Infrastructure/ArchiveUtility';
-export { LocalFileStorage } from './Infrastructure/LocalFileStorage';
-export { inferModelFromFilename, sanitizeId } from './Infrastructure/ModelFileInference';
-// Services
-export { HTTPService } from './services/HTTPService';
-export { AnalyticsEmitter } from './services/AnalyticsEmitter';
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.DEFAULT_EXTENSIONS = void 0;
+Object.defineProperty(exports, "File", {
+  enumerable: true,
+  get: function () {
+    return _file.default;
+  }
+});
+Object.defineProperty(exports, "buildExternalHelpers", {
+  enumerable: true,
+  get: function () {
+    return _buildExternalHelpers.default;
+  }
+});
+Object.defineProperty(exports, "createConfigItem", {
+  enumerable: true,
+  get: function () {
+    return _index2.createConfigItem;
+  }
+});
+Object.defineProperty(exports, "createConfigItemAsync", {
+  enumerable: true,
+  get: function () {
+    return _index2.createConfigItemAsync;
+  }
+});
+Object.defineProperty(exports, "createConfigItemSync", {
+  enumerable: true,
+  get: function () {
+    return _index2.createConfigItemSync;
+  }
+});
+Object.defineProperty(exports, "getEnv", {
+  enumerable: true,
+  get: function () {
+    return _environment.getEnv;
+  }
+});
+Object.defineProperty(exports, "loadOptions", {
+  enumerable: true,
+  get: function () {
+    return _index2.loadOptions;
+  }
+});
+Object.defineProperty(exports, "loadOptionsAsync", {
+  enumerable: true,
+  get: function () {
+    return _index2.loadOptionsAsync;
+  }
+});
+Object.defineProperty(exports, "loadOptionsSync", {
+  enumerable: true,
+  get: function () {
+    return _index2.loadOptionsSync;
+  }
+});
+Object.defineProperty(exports, "loadPartialConfig", {
+  enumerable: true,
+  get: function () {
+    return _index2.loadPartialConfig;
+  }
+});
+Object.defineProperty(exports, "loadPartialConfigAsync", {
+  enumerable: true,
+  get: function () {
+    return _index2.loadPartialConfigAsync;
+  }
+});
+Object.defineProperty(exports, "loadPartialConfigSync", {
+  enumerable: true,
+  get: function () {
+    return _index2.loadPartialConfigSync;
+  }
+});
+Object.defineProperty(exports, "parse", {
+  enumerable: true,
+  get: function () {
+    return _parse.parse;
+  }
+});
+Object.defineProperty(exports, "parseAsync", {
+  enumerable: true,
+  get: function () {
+    return _parse.parseAsync;
+  }
+});
+Object.defineProperty(exports, "parseSync", {
+  enumerable: true,
+  get: function () {
+    return _parse.parseSync;
+  }
+});
+exports.resolvePreset = exports.resolvePlugin = void 0;
+Object.defineProperty((0, exports), "template", {
+  enumerable: true,
+  get: function () {
+    return _template().default;
+  }
+});
+Object.defineProperty((0, exports), "tokTypes", {
+  enumerable: true,
+  get: function () {
+    return _parser().tokTypes;
+  }
+});
+Object.defineProperty(exports, "transform", {
+  enumerable: true,
+  get: function () {
+    return _transform.transform;
+  }
+});
+Object.defineProperty(exports, "transformAsync", {
+  enumerable: true,
+  get: function () {
+    return _transform.transformAsync;
+  }
+});
+Object.defineProperty(exports, "transformFile", {
+  enumerable: true,
+  get: function () {
+    return _transformFile.transformFile;
+  }
+});
+Object.defineProperty(exports, "transformFileAsync", {
+  enumerable: true,
+  get: function () {
+    return _transformFile.transformFileAsync;
+  }
+});
+Object.defineProperty(exports, "transformFileSync", {
+  enumerable: true,
+  get: function () {
+    return _transformFile.transformFileSync;
+  }
+});
+Object.defineProperty(exports, "transformFromAst", {
+  enumerable: true,
+  get: function () {
+    return _transformAst.transformFromAst;
+  }
+});
+Object.defineProperty(exports, "transformFromAstAsync", {
+  enumerable: true,
+  get: function () {
+    return _transformAst.transformFromAstAsync;
+  }
+});
+Object.defineProperty(exports, "transformFromAstSync", {
+  enumerable: true,
+  get: function () {
+    return _transformAst.transformFromAstSync;
+  }
+});
+Object.defineProperty(exports, "transformSync", {
+  enumerable: true,
+  get: function () {
+    return _transform.transformSync;
+  }
+});
+Object.defineProperty((0, exports), "traverse", {
+  enumerable: true,
+  get: function () {
+    return _traverse().default;
+  }
+});
+exports.version = exports.types = void 0;
+var _file = require("./transformation/file/file.js");
+var _buildExternalHelpers = require("./tools/build-external-helpers.js");
+var resolvers = require("./config/files/index.js");
+var _environment = require("./config/helpers/environment.js");
+function _types() {
+  const data = require("@babel/types");
+  _types = function () {
+    return data;
+  };
+  return data;
+}
+Object.defineProperty((0, exports), "types", {
+  enumerable: true,
+  get: function () {
+    return _types();
+  }
+});
+function _parser() {
+  const data = require("@babel/parser");
+  _parser = function () {
+    return data;
+  };
+  return data;
+}
+function _traverse() {
+  const data = require("@babel/traverse");
+  _traverse = function () {
+    return data;
+  };
+  return data;
+}
+function _template() {
+  const data = require("@babel/template");
+  _template = function () {
+    return data;
+  };
+  return data;
+}
+var _index2 = require("./config/index.js");
+var _transform = require("./transform.js");
+var _transformFile = require("./transform-file.js");
+var _transformAst = require("./transform-ast.js");
+var _parse = require("./parse.js");
+const version = exports.version = "7.29.0";
+const resolvePlugin = (name, dirname) => resolvers.resolvePlugin(name, dirname, false).filepath;
+exports.resolvePlugin = resolvePlugin;
+const resolvePreset = (name, dirname) => resolvers.resolvePreset(name, dirname, false).filepath;
+exports.resolvePreset = resolvePreset;
+const DEFAULT_EXTENSIONS = exports.DEFAULT_EXTENSIONS = Object.freeze([".js", ".jsx", ".es6", ".es", ".mjs", ".cjs"]);
+exports.OptionManager = class OptionManager {
+  init(opts) {
+    return (0, _index2.loadOptionsSync)(opts);
+  }
+};
+exports.Plugin = function Plugin(alias) {
+  throw new Error(`The (${alias}) Babel 5 plugin is being run with an unsupported Babel version.`);
+};
+0 && (exports.types = exports.traverse = exports.tokTypes = exports.template = 0);
+
 //# sourceMappingURL=index.js.map
